8bf6f9f4103354096ca28b92008a87b6a2c8c20a
Isaque Ferreira <iffcorreia@gmail.com>
2022-01-18 00:13:35 -0300

    Aprendendo python

--- EX063.py
+++ EX063.py
@@ -2,9 +2,19 @@ num1 = int(input('Digite um valor'))
 loop = 0
 numatual = num1
 numanterior = 0
+repeticao = 10
 
-while loop <= 10:
+while loop < repeticao:
     print(numatual)
     numatual += numanterior
-    numanterior = numatual
+    numanterior = numatual - numanterior
     loop += 1
+    if loop == repeticao:
+        pergunta = input('Você ver mais numeros dessa sequencia, S/N?').strip().lower()
+        if pergunta == 's' or pergunta == 'sim':
+            perguntarepeticao = int(input('Quantos numeros você deseja ver a mais dessa sequencia?'))
+            repeticao += perguntarepeticao
+        elif pergunta == 'n' or pergunta == 'não' or pergunta == 'nao':
+            print('Programa finalizado, obrigado')
+        else:
+            print('Opção invalida, por favor digite sim ou não')
